a98cc8471fa67e12c316e543b7be9afccb34183e
Ludwing Perez <mirage232@gmail.com>
2020-03-03 20:29:14 -0600

    Clase 35: Preparando el backend.
    Mover el formulario de edición/creación de form a un componente independiente.

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,16 +2,19 @@
     <section class="post-list">
             <PostPreview 
                 id=1 
+                :is-admin="isAdmin"
                 thumbnail="https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg"
                 title="Hello there"
                 previewText="This is my first post" />
             <PostPreview 
                 id=2 
+                :is-admin="isAdmin"
                 thumbnail="https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg"
                 title="Hello there - the second time"
                 previewText="This is my second post" />
             <PostPreview 
                 id=3 
+                :is-admin="isAdmin"
                 thumbnail="https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg"
                 title="Hi!"
                 previewText="This is my third post" />
@@ -23,6 +26,12 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
     components: {
         PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
     }
 }
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Ludwing',
+                title: 'My awesome post',
+                content: 'Super amazing, thanks for that!',
+                thumbnailLink: 'https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
