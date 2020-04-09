df69df639b90494ba4e9d6b5574fee6ff179a534
Ludwing Perez <mirage232@gmail.com>
2020-04-08 19:17:14 -0600

    Clase 56: Obtener datos desde backend.

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 // La store debe ser un callable y no un objeto, ya que nuxt lo utiliza en 
 // el servidor para iniciar una store 
@@ -17,39 +18,15 @@ const createStore = () => {
         },
         actions: {
             nuxtServerInit(vuexContext, context) {
-                return new Promise((resolve, reject) => {
-                    const timeWait = Math.floor(Math.random()*(2000-500+1)+500)
-                    setTimeout(() => {
-                        vuexContext.commit('setPosts', [
-                            {
-                                id: "99",
-                                author: 'Ludwing',
-                                title: 'My first dummy post',
-                                content: 'Lorem ipsum dolor sit amet!',
-                                previewText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus turpis...',
-                                thumbnail: 'https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg'
-                            },
-                            {
-                                id: "100",
-                                author: 'Ludwing',
-                                title: 'My second dummy post',
-                                content: 'Lorem ipsum dolor sit amet!',
-                                previewText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus turpis...',
-                                thumbnail: 'https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg'
-                            },
-                            {
-                                id: "101",
-                                author: 'Ludwing',
-                                title: 'Dummy post - the end',
-                                content: 'Lorem ipsum dolor sit amet!',
-                                previewText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus turpis...',
-                                thumbnail: 'https://cnet2.cbsistatic.com/img/qCyN0wPErBBpX1XhxL3dlK9YJrA=/0x188:1732x1397/1092x0/2019/10/24/7a762519-5241-4a3d-a469-0a9c3def3d50/gettyimages-1088374446.jpg'
-                            }
-                        ])
-                        resolve()
-                    }, timeWait)
-                    // reject(new Error())
-                })
+                return axios.get('https://my-nuxt-blog-9b5f5.firebaseio.com/posts.json')
+                    .then(res => {
+                        const postList = []
+                        for (let key in res.data) {
+                            postList.push({...res.data[key], id: key})
+                        }
+                        vuexContext.commit('setPosts', postList)
+                    })
+                    .catch(e => context.error(e))
             },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts)
